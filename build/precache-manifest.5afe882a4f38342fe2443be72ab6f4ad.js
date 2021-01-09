ac68a1100d23a12f3bbd1b2681b1bb170c92e6f0
abul-hasanat <76169950+abul-hasanat@users.noreply.github.com>
2021-01-09 11:02:10 +0600

    live

---
+++ build/precache-manifest.5afe882a4f38342fe2443be72ab6f4ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07267ff161eba80f038963147c1f713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920097b6a742d368b360",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "945a59a358936689d0fb",
+    "url": "/static/css/main.b63b6401.chunk.css"
+  },
+  {
+    "revision": "920097b6a742d368b360",
+    "url": "/static/js/2.3083c010.chunk.js"
+  },
+  {
+    "revision": "e9b70ed94c92560626f6d789c6814c32",
+    "url": "/static/js/2.3083c010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945a59a358936689d0fb",
+    "url": "/static/js/main.7d903baa.chunk.js"
+  },
+  {
+    "revision": "571e0690116d05c07f0f",
+    "url": "/static/js/runtime-main.8b60a04f.js"
+  },
+  {
+    "revision": "b7a4dc2dd0d06e9023531ca1107ba1eb",
+    "url": "/static/media/PetExo-Logo.b7a4dc2d.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "c6b59a8e32004898bbdc78adb83ea403",
+    "url": "/static/media/sidebar-2.c6b59a8e.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
